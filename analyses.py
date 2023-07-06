212dbb5f326644b159d7973dbbbb32cc6c619e9b
Sam Harrison <28861239+sharrison5@users.noreply.github.com>
2023-07-07 10:24:54 +1200

    Move rest of polished figures to `plots.py`
    Still plenty of prototype code in the main script, but now at least a
    clean separation between work in progress and more stable code.

--- analyses.py
+++ analyses.py
@@ -22,7 +22,6 @@ import matplotlib as mpl, matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from pathlib import Path
-import scipy.stats
 
 from solar_analyses import modelling, plots, utilities
 
@@ -47,6 +46,20 @@ df = df.loc[df.index >= "2022-12-21", :]
 figures = {**figures, **plots.plot_raw_data(df)}
 
 # -----------------------------------------------------------------------------
+# Fit the model
+
+stan_fit = modelling.fit_model(df)
+
+# -----------------------------------------------------------------------------
+# Generate summary plots of posterior
+
+figures = {
+    **figures,
+    **plots.plot_optimal_production(df, stan_fit),
+    **plots.plot_weather_effect(df, stan_fit),
+}
+
+# -----------------------------------------------------------------------------
 # Save and display figures
 
 for name, fig in figures.items():
@@ -56,79 +69,7 @@ for name, fig in figures.items():
 plt.show()
 
 # -----------------------------------------------------------------------------
-# Fit the model
-
-stan_fit = modelling.fit_model(df)
-
-# -----------------------------------------------------------------------------
-
-# To do:
-#  Turn weather_effect / optimal production into proper multi-index dataframes
-
-weather_effect = (
-    stan_fit.loc[:, stan_fit.columns.str.startswith("weather_effect")]
-    .to_numpy()
-    .transpose()
-)
-offset_in_year = utilities.date_to_offset_in_year(df.index)
-fig, ax = plt.subplots(figsize=[8.0, 4.0])
-# ax.plot(df.index, weather_effect)
-# ax.plot(df.index, df["Total production"] / 50000, "k")
-# ax.plot(df.index, weather_effect, color=[0.7] * 3, linewidth=0.2)
-# ax.plot(
-#    df.index,
-#    np.quantile(weather_effect, [0.25, 0.75], axis=1).transpose(),
-#    color=[0.4] * 3,
-# )
-# ax.plot(df.index, np.median(weather_effect, axis=1), "k")
-ax.bar(
-    df.index,
-    np.median(weather_effect, axis=1),
-    yerr=np.abs(
-        np.median(weather_effect, axis=1)
-        - np.quantile(weather_effect, [0.25, 0.75], axis=1)
-    ),
-)
-ax.plot(
-    df.index,
-    0.5 + 0.3 * np.cos(2.0 * math.pi * offset_in_year),
-    "w",
-    linewidth=3.0,
-)
-ax.plot(df.index, 0.5 + 0.3 * np.cos(2.0 * math.pi * offset_in_year), "r")
-# ax.plot(
-#    df.index,
-#    0.8 * df["Total production"] / np.max(df["Total production"]), "r")
-ax.set_xlabel("Date")
-ax.set_ylabel("Weather effect")
-fig.autofmt_xdate()
-ax.set_ylim(0.0, 1.0)
-
-plt.savefig("Figures/WeatherEffect.jpg")
-plt.savefig("Figures/WeatherEffect.pdf")
-
-fig, ax = plt.subplots()
-h1 = ax.hist(
-    weather_effect.flatten(),
-    bins=np.linspace(0.0, 1.0, 50),
-    density=True,
-    rwidth=0.9,
-    label="Posterior samples",
-)
-x = np.linspace(0.0001, 0.9999, 500)
-p = 0.25
-h2 = ax.plot(
-    x,
-    (1.0 - p) * scipy.stats.beta.pdf(x, 2.0, 2.0)
-    + p * scipy.stats.beta.pdf(x, 10.0, 1.0),
-    label="Prior",
-)
-ax.set_xlabel("Weather effect")
-ax.set_ylabel("Probability density")
-ax.legend()
-
-plt.savefig("Figures/WeatherEffectDistribution.jpg")
-plt.savefig("Figures/WeatherEffectDistribution.pdf")
+# Prototype code
 
 # for n in range(10):
 #     plt.figure()
@@ -145,28 +86,6 @@ plt.savefig("Figures/WeatherEffectDistribution.pdf")
 #         (1.0 - p) * scipy.stats.beta.pdf(x, 2.0, 2.0)
 #         + p * scipy.stats.beta.pdf(x, 10.0, 1.0),
 #     )
-
-optimal_production = (
-    stan_fit.loc[:, stan_fit.columns.str.startswith("optimal_production")]
-    .to_numpy()
-    .transpose()
-)
-fig, ax = plt.subplots(figsize=[8.0, 4.0])
-# ax.plot(df.index, df["Total production"])
-ax.bar(df.index, df["Total production"])
-ax.plot(df.index, optimal_production, color=[0.7] * 3, linewidth=0.2)
-ax.plot(
-    df.index,
-    np.quantile(optimal_production, [0.25, 0.75], axis=1).transpose(),
-    color=[0.4] * 3,
-)
-ax.plot(df.index, np.median(optimal_production, axis=1), "r")
-ax.set_xlabel("Date")
-ax.set_ylabel("Production (kWh)")
-fig.autofmt_xdate()
-
-plt.savefig("Figures/OptimalProduction.jpg")
-plt.savefig("Figures/OptimalProduction.pdf")
 
 plt.figure()
 plt.hist(stan_fit["max"], bins=99)
@@ -200,3 +119,5 @@ ax.xaxis.set_major_formatter(mpl.dates.DateFormatter("%d-%b"))
 fig.autofmt_xdate()
 
 plt.show()
+
+# -----------------------------------------------------------------------------
--- solar_analyses/plots.py
+++ solar_analyses/plots.py
@@ -16,7 +16,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import math
 import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats
+
+from solar_analyses import utilities
 
 # -----------------------------------------------------------------------------
 
@@ -35,6 +40,107 @@ def plot_raw_data(df):
     ax.legend()
     fig.autofmt_xdate()
     figures["production"] = fig
+
+    return figures
+
+
+# -----------------------------------------------------------------------------
+
+# To do:
+#  Turn weather_effect / optimal production into proper multi-index dataframes
+
+
+def plot_optimal_production(df, stan_fit):
+    """Summarise the posterior over the weather effect parameters."""
+
+    figures = {}
+
+    optimal_production = (
+        stan_fit.loc[:, stan_fit.columns.str.startswith("optimal_production")]
+        .to_numpy()
+        .transpose()
+    )
+
+    fig, ax = plt.subplots(figsize=[8.0, 4.0])
+    # Raw data
+    ax.bar(df.index, df["Total production"])
+    # Distribution of optimal production over time
+    ax.plot(df.index, optimal_production, color=[0.7] * 3, linewidth=0.2)
+    ax.plot(
+        df.index,
+        np.quantile(optimal_production, [0.25, 0.75], axis=1).transpose(),
+        color=[0.4] * 3,
+    )
+    ax.plot(df.index, np.median(optimal_production, axis=1), "r")
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Production (kWh)")
+    fig.autofmt_xdate()
+
+    figures["optimal_production"] = fig
+
+    return figures
+
+
+# -----------------------------------------------------------------------------
+
+
+def plot_weather_effect(df, stan_fit):
+    """Summarise the posterior over the weather effect parameters."""
+
+    figures = {}
+
+    weather_effect = (
+        stan_fit.loc[:, stan_fit.columns.str.startswith("weather_effect")]
+        .to_numpy()
+        .transpose()
+    )
+    offset_in_year = utilities.date_to_offset_in_year(df.index)
+
+    # Plot weather effect over time
+    fig, ax = plt.subplots(figsize=[8.0, 4.0])
+    ax.bar(
+        df.index,
+        np.median(weather_effect, axis=1),
+        yerr=np.abs(
+            np.median(weather_effect, axis=1)
+            - np.quantile(weather_effect, [0.25, 0.75], axis=1)
+        ),
+    )
+    # Plot the seasonality as a sinusoid
+    ax.plot(
+        df.index,
+        0.5 + 0.3 * np.cos(2.0 * math.pi * offset_in_year),
+        "w",
+        linewidth=3.0,
+    )
+    ax.plot(df.index, 0.5 + 0.3 * np.cos(2.0 * math.pi * offset_in_year), "r")
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Weather effect")
+    fig.autofmt_xdate()
+    ax.set_ylim(0.0, 1.0)
+    figures["weather_effect"] = fig
+
+    # Compare marginal distribution to prior
+    fig, ax = plt.subplots()
+    ax.hist(
+        weather_effect.flatten(),
+        bins=np.linspace(0.0, 1.0, 50),
+        density=True,
+        rwidth=0.9,
+        label="Posterior samples",
+    )
+    x = np.linspace(0.0001, 0.9999, 500)
+    p = 0.25
+    ax.plot(
+        x,
+        (1.0 - p) * scipy.stats.beta.pdf(x, 2.0, 2.0)
+        + p * scipy.stats.beta.pdf(x, 10.0, 1.0),
+        label="Prior",
+    )
+    ax.set_xlabel("Weather effect")
+    ax.set_ylabel("Probability density")
+    ax.legend()
+    figures["weather_effect_distribution"] = fig
 
     return figures
 
